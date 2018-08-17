3d69f0050ec49555348f76e7f1ca3bc978fab944
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-08-17 12:56:48 -0700

    add "init", "today" as valid arguments

--- journal.sh
+++ journal.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 NOTES="$HOME/Dropbox/Apps/Editorial/_notes/_journal"
+if [ ! -e "$PWD/.journalrc" ]; then touch "$PWD/.journalrc"; fi
+rc=$PWD/.journalrc
 if [ $# -eq 0 ]; then
-	if [ ! -e "$NOTES/$(date +%Y%m%d).md" ]
-	then
-		touch "$NOTES/$(date +%Y%m%d).md"
-		echo "Created today's journal entry!"
-	else
-		echo "Today's journal entry already exists!"
-	fi
+	echo "Usage: journal [command]"
 else 
-	if [ $# -eq 1 ]; then
-		echo $1
+	if [ $1 = 'today' ]; then 
+		if [ ! -e "$NOTES/$(date +%Y%m%d).md" ]
+		then
+			touch "$NOTES/$(date +%Y%m%d).md"
+			echo "Created today's journal entry!"
+		else
+			echo "Today's journal entry already exists!"
+		fi
+	fi
+	if [ $1 = 'init' ]; then
+		hash=$(echo $PWD | md5)
+		if [ ! $(grep -co $hash $rc) -eq 0 ]; then
+			echo "fatal: Already a journal at \"$PWD\""
+		else
+			echo $hash >> $rc
+			echo "Initialized new journal at \"$PWD\""
+		fi
 	fi
 fi
